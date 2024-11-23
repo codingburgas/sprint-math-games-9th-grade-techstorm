c0fe0d6e17ea9e60dddf1375116ac9427a0eba1d
RKKyrilova23 <RKKyrilova23@codingburgas.bg>
2024-11-23 19:09:38 +0200

    update axe_game

--- axe_game.cpp
+++ axe_game.cpp
@@ -6,56 +6,77 @@ int main()
     //window dimensions
     int width{800};
     int height{450};
-    InitWindow(width,height,"Raya");
+    InitWindow(width,height,"Axe_Game");
 
     //circle cordinats
     int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+
+    //circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
 
     //axe cordinats
     int axe_x{400};
     int axe_y{0};
+    int axe_lenght{50};
+
+    //axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_lenght};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_lenght};
 
     int direction{10};
+
+    bool collision_with_axe {false};
 
     SetTargetFPS(60);
      while (WindowShouldClose() == false )
     {
         BeginDrawing();
         ClearBackground(WHITE);
-
-        //Game logig begins
-
-        DrawCircle (circle_x,circle_y,25,BLUE);
-          }
-        DrawRectangle(axe_x,axe_y,50,50,RED);
-
-        //move the axe
-
-        axe_y += direction;
-        if (axe_y > 450)
+        if (collision_with_axe)
         {
-            direction = -direction;
+            DrawText("Game Over!",400,200,20,RED);
         }
+        else
+        {
+                //Game logig begins
+
+            DrawCircle (circle_x,circle_y,circle_radius,BLUE);
+            DrawRectangle(axe_x,axe_y,axe_lenght,axe_lenght,RED);
+
+            //move the axe
+
+            axe_y = axe_y + direction;
+            if (axe_y > height || axe_y < 0)
+            {
+                direction = -direction;
+            }
+            
+
+            if (IsKeyDown(KEY_D) && circle_x < width)
+            {
+                circle_x = circle_x + 10;
+            }
+            if (IsKeyDown(KEY_A) && circle_x > 0)
+            {
+                circle_x = circle_x - 10;
+            }
         
 
-        if (IsKeyDown(KEY_D) && circle_x < 350)
-        {
-            circle_x += 10;
+            //Her Game logig ends
         }
-        if (IsKeyDown(KEY_A) && circle_x > 0)
-        {
-            circle_x -= 10;
-        }
-        DrawRectangle(axe_x,axe_y,50,50,RED);
-        //move the axe
-        axe_y += 10;
         
         
 
-        //Her Game logig ends
+       
 
         EndDrawing();
+     }
     }
-   
-}
